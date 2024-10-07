ffee8eb72ce1582c2bcd4205581005ddd9aaddeb
amoako419 <ghheskey@gmail.com>
2024-10-07 18:54:17 +0000

    else if added

--- array_with_if.playground/Contents.swift
+++ array_with_if.playground/Contents.swift
@@ -1,8 +1,20 @@
 // Array holding mission critical supplies
+//let missionSupplies = ["Oxygen Tanks", "Food Supplies", "Water Packs"]
+//
+//if missionSupplies.contains("Oxygen Tanks") && missionSupplies.contains("Food Supplies") {
+//    print("All critical supplies are loaded.")
+//} else {
+//    print("Check mission supplies.")
+//}
+
+
+
 let missionSupplies = ["Oxygen Tanks", "Food Supplies", "Water Packs"]
 
-if missionSupplies.contains("Oxygen Tanks") && missionSupplies.contains("Food Supplies") {
+if missionSupplies.contains("Water Packs") && missionSupplies.contains("First Aid Kits") {
     print("All critical supplies are loaded.")
+} else if missionSupplies.contains("Water Packs") || missionSupplies.contains("First Aid Kits") {
+    print("At least one critical supply is loaded.")
 } else {
     print("Check mission supplies.")
 }
